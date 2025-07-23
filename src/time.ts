cfd019db2b818df2726057fc9c0fe6e92d85cd24
mur <mur@qq.com>
2025-07-24 07:13:36 +0800

    33

--- src/time.ts
+++ src/time.ts
@@ -8,25 +8,26 @@ export const unixNow = () => {
 
 /**
  * 格式化日期
- * @param date 待格式化的日期对象 
+ * @param date 待格式化的日期对象，或 Unix时间戳，单位为秒
  * @param format 格式化字符串，如"yyyy-MM-dd HH:mm:ss"
  * @returns 格式化后的日期字符串
  */
-export const formatDate = (date: Date, format: string) => {
+export const formatDate = (date: Date | number, format: string) => {
+    const fnDate = typeof date === "number" ? new Date(date) : date; 
     return format.replace(/yyyy|MM|dd|HH|mm|ss/g, (match) => {
         switch (match) {
             case "yyyy":
-                return date.getFullYear().toString();
+                return fnDate.getFullYear().toString();
             case "MM":
-                return (date.getMonth() + 1).toString();
+                return (fnDate.getMonth() + 1).toString();
             case "dd":
-                return date.getDate().toString();
+                return fnDate.getDate().toString();
             case "HH":
-                return date.getHours().toString();
+                return fnDate.getHours().toString();
             case "mm":
-                return date.getMinutes().toString();
+                return fnDate.getMinutes().toString();
             case "ss":
-                return date.getSeconds().toString();
+                return fnDate.getSeconds().toString();
             default:
                 return "";
         }
